4c453097818740ab6ff213d3ec9e30d00bfae027
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-07-15 00:19:46 -0300

    add

--- source/mode.py
+++ source/mode.py
@@ -1,4 +1,4 @@
-import math
+from math import ceil
 from history import saveHistory
 from tkinter import messagebox
 
@@ -9,9 +9,7 @@ class Calculator():
             set variables
         '''
         self.floatValue = 0
-        self.percent = 0
-        self.value = 0
-        self.var = 0
+    
     
     def calc(self, option, value):
         '''
@@ -20,39 +18,45 @@ class Calculator():
             set value(entryValue) as a float
             identify the radioOption(option) and asign it with a percent
         '''
-
-        if value is None or value == '':
-            return ''
         
         try:
-            self.floatValue = float(value)
+            if not value.strip():
+                return ''
 
-            if option == 0: 
-                discount = 1 - 0.15
-                percent = "15%"
+            self.floatValue = float(value.strip())
 
-            elif option == 1:  
-                discount = 1 - 0.20
-                percent = "20%"
+            if self.floatValue <= 0 or self.floatValue > 999999:
+                messagebox.showerror('erro', 'valor não permitido')
+                return ''
+            
+            discounts = {
+                0: (0.15, '15%'),
+                1: (0.25, '25%'),
+                2: (0.35, '35%'),
+            }
 
-            elif option == 2:
-                discount = 1 - 0.35
-                percent = "35%"
-                
+            if option in discounts:
+                discount, percent = discounts[option]
+            else:
+                messagebox.showerror('erro','Opção inválida')
+    
             '''
                 basic calc is how percent works in real life, outcome is a math ceil to round the result and avoid lack 0,1 cent in the operation
             '''    
-            var = self.floatValue / discount
-            outcome = math.ceil(var * 100) / 100
-            saveHistory(value, outcome, percent)
+            varCalc = self.floatValue / (1 - discount)
+
+            outcome = ceil(varCalc * 100) / 100
+
+            saveHistory(self.floatValue, outcome, percent)
             return outcome
+        
         except:
             messagebox.showerror('erro', 'Valor inválido')
             return ''
 
-    ## FUNÇÃO QUE CALCULO O VALOR PARA OFERTA MELI
     def offer(self):
-        return self.floatValue - (self.floatValue * 0.03) 
+
+        return round(self.floatValue - (self.floatValue * 0.03))
     
 
     ## CÁLCULO DO ESTOQUE FULL POR SEMANA
